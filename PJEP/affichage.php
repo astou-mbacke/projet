22b8b160a8b91d8c56ec79a855a00ba25cf0dc61
astou-mbacke <you@example.com>
2021-12-09 13:20:31 +0100

    projet bootcamp

---
+++ PJEP/affichage.php
@@ -0,0 +1,18 @@
+<!DOCYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+<?php
+echo"ceci est une ligne uniquement creer en PHP";
+echo"ceci est la 2em phrase creer qvec PHP";
+
+?>
+</body>
+</html>
